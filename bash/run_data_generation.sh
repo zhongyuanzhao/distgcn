5f53388c7e3eaa65e17259acc6b3a4ae12da7962
zhongyuanzhao <zhongyuan.zhao@rice.edu>
2020-11-21 15:20:59 -0600

    script for data generation

--- bash/run_data_generation.sh
+++ bash/run_data_generation.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
-python3 Data_Generation.py --datapath ./data/ER_Graph_Uniform > log_data_generation_uniform.txt;
-echo "Uniform Weighted Graph Generation Completed";
-python3 Data_Generation.py --datapath ./data/ER_Graph_Normal_l1 --dist normal_l1 > log_data_generation_normal_l1.txt;
-echo "Normal L1 Weighted Graph Generation Completed";
-python3 Data_Generation.py --datapath ./data/ER_Graph_Normal_l2 --dist normal_l2 > log_data_generation_normal_l2.txt;
-echo "Normal L2 Weighted Graph Generation Completed";
 
-python3 Data_Generation.py --datapath ./data/Ramdom_Graph_NB100_test --n 50 --nbs "100" > log_data_generation_uniform.txt;
+dist='Uniform';
 
-python3 Data_Generation.py --datapath ./data/ER_Graph_Uniform_N200_h0 --n 1000 --sizes "200" --nbs "20,40,60,80,100,120,140,160,180" > log_data_generation_uniform.txt;
+for graph in 'ER' 'BA'; do
 
-python3 Data_Generation.py --datapath ./data/ER_Graph_Uniform_N200 --n 2 --sizes "20" --nbs "2,4,6,8,10,12,14,16,18" > log_data_generation_uniform.txt;
+	train_data="${graph}_Graph_${dist}_mixN_mixp_train0";
+	if [ -d "./data/${train_data}" ]; then
+		echo "./data/${train_data} already exists, skip generation.";
+	else
+		echo "$Start generating training dataset: ./data/{train_data} .";
+		python3 Data_Generation.py --datapath ./data/${train_data} --n 200 --sizes "50,100,150,200,250" --ps "0.02,0.05,0.075,0.10,0.15" --type "${graph}"  --dist "${dist}" > log_data_generation_${train_data}.txt;
+		python3 Data_Generation.py --datapath ./data/${train_data} --n 20 --sizes "50,100,150,200,250" --ps "0.90,0.80,0.70,0.60,0.50,0.40,0.30" --type "${graph}" --dist "normal_l2" >> log_data_generation_${train_data}.txt;
+		python3 Data_Generation.py --datapath ./data/${train_data} --n 30 --sizes "20" --nbs "18,16,14,12,10,8,6,4,2" --type "${graph}" --dist "normal_l2" >> log_data_generation_${train_data}.txt;
+	fi
+
+	testfolder="${graph}_Graph_${dist}_GEN21_test2";
+	if [ -d "./data/${testfolder}" ]; then
+		echo "./data/${testfolder} already exist, skip generation";
+	else
+		echo "$Start generating testing dataset: ./data/{testfolder} .";
+		python3 Data_Generation.py --datapath ./data/${testfolder} --n 20 --sizes "100" --nbs "2,5,10,15,20" --type "${graph}" --dist "${dist}" >> log_data_generation_${testfolder}.txt;
+		python3 Data_Generation.py --datapath ./data/${testfolder} --n 20 --sizes "150" --nbs "2,5,10,15,20" --type "${graph}" --dist "${dist}" >> log_data_generation_${testfolder}.txt;
+		python3 Data_Generation.py --datapath ./data/${testfolder} --n 20 --sizes "200" --nbs "2,5,10,15,20" --type "${graph}" --dist "${dist}" >> log_data_generation_${testfolder}.txt;
+		python3 Data_Generation.py --datapath ./data/${testfolder} --n 20 --sizes "250" --nbs "2,5,10,15,20" --type "${graph}" --dist "${dist}" >> log_data_generation_${testfolder}.txt;
+		python3 Data_Generation.py --datapath ./data/${testfolder} --n 20 --sizes "300" --nbs "2,5,10,15,20" --type "${graph}" --dist "${dist}" >> log_data_generation_${testfolder}.txt;
+	fi
+
+done
